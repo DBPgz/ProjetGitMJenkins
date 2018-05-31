86c0a14af804f9a1265a0c3f2fe184057b99ca1f
DBPgz <pougaza@gmail.com>
2018-05-31 10:22:39 +0200

    mon fichier compteur

---
+++ src/main/java/futurskill/Compteur.java
@@ -0,0 +1,17 @@
+package futurskill;
+
+
+public class Compteur {
+  int Count=0;
+  int Increment() {
+	  return ++ Count ;
+  }
+  int Decrement() {
+	  return -- Count;
+  }
+  int getValeur() {
+	return Count;
+	  
+  }
+  
+}
